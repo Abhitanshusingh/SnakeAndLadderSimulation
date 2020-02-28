8a3ffea9074ec8b44a34e79688c54ae2b29827fe
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-28 17:32:03 +0530

    MODIFIED: Check for option no player ladder or snake

--- SnakeAndLadderSimulation.sh
+++ SnakeAndLadderSimulation.sh
@@ -3,4 +3,26 @@ echo "Welcome to snake and ladder simulator"
 #CONSTANT VARIABLE
 #PLAYER1 STARTING POSITION
 PLAYER1=0
-PLAYER1=$((RANDOM%6+1))
+#GENERATING RANDOM VALUE 1 TO 6
+dice=$((RANDOM%6+1))
+START_POSITION=0
+#GENERATING RANDOM VALUE TO CHECK
+CheckOption=$((RANDOM%3+1))
+case $CheckOption in
+	1)
+		#NO PLAYER
+		PLAYER1=$PLAYER1
+		echo "Player in same position: $PLAYER1"
+		;;
+
+	2)
+		#PLAYER MOVES AHEAD
+		PLAYER1=$(($PLAYER1+$dice))
+		echo "Player move ahead  in position: $PLAYER1"
+		;;
+	3)
+		#PLAYUER MOVES BEHIND
+		PLAYER1=$(($PLAYER-$dice))
+		echo "Player move behind in position: $PLAYER1"
+		;;
+esac
