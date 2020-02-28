03321799b45f3cb802842a2bfc35c9a231f27732
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-28 18:16:45 +0530

    MODIFIED: Repeat till the player win

--- SnakeAndLadderSimulation.sh
+++ SnakeAndLadderSimulation.sh
@@ -3,26 +3,34 @@ echo "Welcome to snake and ladder simulator"
 #CONSTANT VARIABLE
 #PLAYER1 STARTING POSITION
 PLAYER1=0
-#GENERATING RANDOM VALUE 1 TO 6
-dice=$((RANDOM%6+1))
-START_POSITION=0
-#GENERATING RANDOM VALUE TO CHECK
-CheckOption=$((RANDOM%3+1))
-case $CheckOption in
-	1)
-		#NO PLAYER
-		PLAYER1=$PLAYER1
-		echo "Player in same position: $PLAYER1"
-		;;
+while(($PLAYER1<100))
+do
+	#GENERATING RANDOM VALUE 1 TO 6
+	dice=$((RANDOM%6+1))
+	#GENERATING RANDOM VALUE TO CHECK
+	CheckOption=$((RANDOM%3+1))
+	case $CheckOption in
+		1)
+			#NO PLAYER
+			PLAYER1=$PLAYER1
+			echo "Player in same position: $PLAYER1"
+			;;
 
-	2)
-		#PLAYER MOVES AHEAD
-		PLAYER1=$(($PLAYER1+$dice))
-		echo "Player move ahead  in position: $PLAYER1"
-		;;
-	3)
-		#PLAYER MOVES BEHIND
-		PLAYER1=$(($PLAYER-$dice))
-		echo "Player move behind in position: $PLAYER1"
-		;;
-esac
+		2)
+			#PLAYER MOVES AHEAD
+			PLAYER1=$(($PLAYER1+$dice))
+			echo "Player move ahead  in position: $PLAYER1"
+			;;
+		3)
+			#PLAYER MOVES BEHIND
+			PLAYER1=$(($PLAYER1-$dice))
+			echo "Player move behind in position: $PLAYER1"
+			#WHEN PLAYER IS LESS THEN 0
+			if(($PLAYER1<0))
+      	then
+         	PLAYER1=0
+			fi
+			;;
+	esac
+
+done
