53acc55c1f0d747cc86347c7865267fa29448734
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-28 19:27:35 +0530

    MODIFIED: Counting dice every time and position of dice roll

--- SnakeAndLadderSimulation.sh
+++ SnakeAndLadderSimulation.sh
@@ -1,34 +1,38 @@
 #!/bin/bash -x
 echo "Welcome to snake and ladder simulator"
-#CONSTANT VARIABLE
 #PLAYER1 STARTING POSITION
 player1=0
+countDice=0
 while(($player1<100))
 do
 	#PREVIOUS POSITION
 	previous_position=$player1
 	#GENERATING RANDOM VALUE 1 TO 6
 	dice=$((RANDOM%6+1))
+	countDice=$(($countDice+1))
 	#GENERATING RANDOM VALUE TO CHECK
-	CheckOption=$((RANDOM%3+1))
-	case $CheckOption in
+	checkOption=$((RANDOM%3+1))
+	case $checkOption in
 		1)
 			#NO PLAYER
 			player1=$player1
+			echo "Dice position after role : $dice"
 			echo "Player in same position: $player1"
 			;;
 		2)
 			#PLAYER MOVES AHEAD
 			player1=$(($player1+$dice))
 			if(($player1>100))
-         then
-            player1=$previous_position
-         fi
+			then
+				player1=$previous_position
+			fi
+			echo "Dice position after role : $dice"
 			echo "Player move ahead  in position: $player1"
 			;;
 		3)
 			#PLAYER MOVES BEHIND
 			player1=$(($player1-$dice))
+			echo "Dice position after role : $dice"
 			echo "Player move behind in position: $player1"
 			#WHEN PLAYER IS LESS THEN 0
 			if(($player1<0))
@@ -40,3 +44,4 @@ do
 	esac
 
 done
+echo "Number of time the dice was played $countDice"
