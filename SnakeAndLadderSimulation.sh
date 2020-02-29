ca604ecc907feb25664fc1044f62682c14a041e6
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-02-29 15:14:00 +0530

    MODIFIED: one and two player

--- SnakeAndLadderSimulation.sh
+++ SnakeAndLadderSimulation.sh
@@ -1,47 +1,76 @@
 #!/bin/bash -x
 echo "Welcome to snake and ladder simulator"
-#PLAYER1 STARTING POSITION
-player1=0
-countDice=0
-while(($player1<100))
-do
+player=0
+countDice1=0
+countDice2=0
+dicePosition1=0
+dicePosition2=0
+function snakeAndLadder ()
+{
+	player=$1
+	#PLAYER1 STARTING POSITION
 	#PREVIOUS POSITION
-	previous_position=$player1
+	previousPosition=$player
 	#GENERATING RANDOM VALUE 1 TO 6
 	dice=$((RANDOM%6+1))
 	countDice=$(($countDice+1))
-	#GENERATING RANDOM VALUE TO CHECK
+	#GENERATING RANDOM VALUE TO CHECK IF 1 THEN SAME POSITION 2 THEN LADDER AND 3 THEN SNAKE
 	checkOption=$((RANDOM%3+1))
 	case $checkOption in
 		1)
 			#NO PLAYER
-			player1=$player1
-			echo "Dice position after role : $dice"
-			echo "Player in same position: $player1"
+			player=$player
 			;;
 		2)
 			#PLAYER MOVES AHEAD
-			player1=$(($player1+$dice))
-			if(($player1>100))
+			player=$(($player+$dice))
+			if(($player>100))
 			then
-				player1=$previous_position
+				player=$previousPosition
 			fi
-			echo "Dice position after role : $dice"
-			echo "Player move ahead  in position: $player1"
 			;;
 		3)
 			#PLAYER MOVES BEHIND
-			player1=$(($player1-$dice))
-			echo "Dice position after role : $dice"
-			echo "Player move behind in position: $player1"
+			player=$(($player-$dice))
 			#WHEN PLAYER IS LESS THEN 0
-			if(($player1<0))
+			if(($player<0))
 			then
-				player1=0
+				player=0
 			fi
 			;;
-
 	esac
-
-done
-echo "Number of time the dice was played $countDice"
+	echo $player
+}
+printf "Enter 1 for single player\n"
+printf "Enter 2 for double player\n"
+read userInput
+case $userInput in
+	1)
+		while(($dicePosition1<100))
+		do
+			dicePosition1="$(snakeAndLadder $dicePosition1)"            #CALLING FUNCTION
+			countDice1=$(($countDice1+1))
+			printf "Player $countDice1 : Position $dicePosition1\n"
+		done
+		printf "\t\t\t\t\t***Congratulations Player Won***\n"
+		;;
+	2)
+		while(($dicePosition1 < 100 && $dicePosition2 < 100))
+		do
+		dicePosition1="$(snakeAndLadder $dicePosition1)"  				#CALLING FUNCTION
+		countDice1=$(($countDice1+1))
+		printf "Player1 $countDice1 : Position $dicePosition1\n"
+		dicePosition2="$(snakeAndLadder $dicePosition2)"				#CALLING FUNCTION
+		countDice2=$(($countDice2+1))
+		printf "Player2 $countDice2 : Position $dicePosition2\n"
+		done
+		if(($dicePosition1==100))
+		then
+			printf "\t\t\t\t\t***Congratulations Player1 Won***\n"
+			printf "\t\t\t\t\tPlayer1 roll the diece for $countDice1 times\n"
+		else
+			printf "\t\t\t\t\t***Congratulations Player2 Won***\n"
+			printf "\t\t\t\\t\tPlayer2 roll the diece for $countDice2 times\n"
+		fi
+		;;
+esac
